b8da020fe61d253b5fb1b86dc6167aa1643ebdd1
jackshawn <jackshawn@live.com>
2021-11-10 10:27:27 +0800

    优化加载速度

--- app.js
+++ app.js
@@ -5,30 +5,44 @@ const axios = require('axios');
 
 app.use(express.static('public'))
 
-app.get('/status', async (req, res) => {
-    
-    let d = await axios.get(`https://miningpoolstats.stream/data/time?t=${new Date().getTime()}`)
-    let data1 = await axios.get(`https://data.miningpoolstats.stream/data/ethereum.js?t=${d.data}`)
-    let data2 = await axios.get(`https://data.miningpoolstats.stream/data/history/ethereum.js?t=${d.data}`)
-    let data3 = await axios.get('https://api.ethermine.org/poolStats')
+let history = {}
 
-    console.log(data3.data)
+app.get('/status', async (req, res) => {
+
+    let d = await axios.get(`https://miningpoolstats.stream/data/time?t=${new Date().getTime()}`);
+    let { data } = await axios.get(`https://data.miningpoolstats.stream/data/ethereum.js?t=${d.data}`)
+
     res.json({
-        status: {
-            usd: data1.data.price,
-            ethHour: data1.data.minerstat[0],
-            cny: data3.data.data.price.cny,
-            gas: data3.data.data.estimates.gasPrice,
-            hashrate: (data1.data.hashrate / 1e12).toFixed(2),
-            difficulty: (data1.data.difficulty / 1e15).toFixed(2),
-        },
-        history: {
-            count: data2.data.count,
-            price: data2.data.history.price,
-            difficulty: data2.data.history.difficulty.map(i => i / 1e15),
-            time: data2.data.history.time
-        }
+        usd: data.price,
+        ethHour: data.minerstat[0],
+        hashrate: (data.hashrate / 1e12).toFixed(2),
+        difficulty: (data.difficulty / 1e15).toFixed(2),
     })
+})
+
+app.get('/history', async (req, res) => {
+
+    let date = `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}`
+
+    // 存在内存中
+    if (history[date]) {
+        res.json(history[date])
+    } else {
+        history = {}
+
+        // get
+        let d = await axios.get(`https://miningpoolstats.stream/data/time?t=${new Date().getTime()}`);
+        let { data } = await axios.get(`https://data.miningpoolstats.stream/data/history/ethereum.js?t=${d.data}`)
+        let result = {
+            count: data.count,
+            price: data.history.price,
+            difficulty: data.history.difficulty.map(i => i / 1e15),
+            time: data.history.time
+        }
+
+        history[date] = result;
+        res.json(result)
+    }
 })
 
 
